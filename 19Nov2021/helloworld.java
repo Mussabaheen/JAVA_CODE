f14f387cc822cd49e4f6d6c247e9c8ae13d1352e
Mussabaheen <mussabaheen@gmail.com>
2021-11-19 23:56:51 +0500

    running code

---
+++ 19Nov2021/helloworld.java
@@ -0,0 +1,52 @@
+public class helloworld {
+    // -> main() -> program start
+    // 
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello World from saad");
+        String saad = "1";
+        int hamza = 132;
+        double zahid = 123.41;
+        char voli = '1';
+        boolean adnan = false;
+                 //123  //123.41
+        int sds = (int) zahid;
+        
+        
+        double volicheck = (double) hamza;
+
+        System.out.println(volicheck);
+
+
+
+
+
+        int sum1 = 132 + 30;  // hamza -> 162 // voli -> 162 // saad -> 162
+        int sum2 = sum1 + sum1 + 30; // hamza -> 354 // voli -> 354 // saad -> 354
+        int sum3 = sum1 + sum2 + 30; // hamza -> 546 // voli -> 546 // saad -> 546
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+        int x = 5;
+        x = x + 5;
+
+        int y = 5;
+        y =y + 30; // y->
+        System.out.println(y<=31);
+        System.out.println(x >= 10);
+        System.out.println(y <= 31 || x >= 10);
+
+        System.out.println(!adnan);
+
+       
+
+
+
+
+
+    }
+
+    
+}
